6408c2e3b7a7c236f0846f91d8f93843f46047e9
arnavpaniya <arnavpaniya@gmail.com>
2026-05-26 01:33:44 +0530

    refactor: update Icon prop type to any in BentoCard component

--- components/ui/bento-grid.tsx
+++ components/ui/bento-grid.tsx
@@ -13,7 +13,7 @@ interface BentoCardProps extends ComponentPropsWithoutRef<"div"> {
   name: string
   className: string
   background: ReactNode
-  Icon: React.ElementType<any>
+  Icon: any
   description: string
   href: string
   cta: string
