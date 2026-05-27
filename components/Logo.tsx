086285b0f811329155ce76d1eb26253bb0e1224d
arnavpaniya <arnavpaniya@gmail.com>
2026-05-27 19:14:05 +0530

    feat: update site logo to logo-black.png

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -7,7 +7,7 @@ export function Logo() {
   return (
     <Link href="/" className="flex items-center justify-center transition-transform duration-300 hover:scale-105 active:scale-95">
       <Image 
-        src="/logo.png" 
+        src="/logo-black.png" 
         alt="ReWorks Logo" 
         width={300} 
         height={300} 
